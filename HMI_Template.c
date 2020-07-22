54099245f77ffda8392251b0ee5708c33f9c964b
Thomas Joseph <1600440@eng.asu.edu.eg>
2020-07-22 18:01:34 +0200

    added Task5

--- HMI_Template.c
+++ HMI_Template.c
@@ -4,23 +4,13 @@
  * \brief Rte Component Template for AUTOSAR SWC: HMI
  *
  * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
- * Generated on 7/16/2020 05:30 PM
+ * Generated on 7/22/2020 03:55 PM
  *
  * For any inquiries: hassan.m.farahat@gmail.com
  *
  */
 
 #include "Rte_HMI.h"
-
-
-/**
- *
- * Runnable HMI_MainFunction
- *
- * Triggered By:
- *  - TimingEventImpl.TE_HMI_MainFunction_100ms
- *
- */
 
 static MultiStateBtn BtnValueToState (uint8 value)
 {
@@ -42,6 +32,16 @@ static MultiStateBtn BtnValueToState (uint8 value)
 		return btnState;
 	}
 }
+
+
+/**
+ *
+ * Runnable HMI_MainFunction
+ *
+ * Triggered By:
+ *  - TimingEventImpl.TE_HMI_MainFunction_100ms
+ *
+ */
 
 void HMI_MainFunction (void)
 {
@@ -77,5 +77,47 @@ void HMI_MainFunction (void)
 		SlideBtnState = BtnValueToState(Slide);
 		(void)Rte_Write_ppSeatCtrlBtns_SlideBtnState(SlideBtnState);
 	}
+}
+
+
+
+/**
+ *
+ * Runnable HMI_SeatModeChanged
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatModeBtn_SeatModeBtn
+ *
+ */
+
+void HMI_SeatModeChanged (void)
+{
+	Std_ReturnType status;
+	SeatModeBtnType SeatModeBtn;
+	uint8 SeatCtrlMode = RTE_MODE_SeatCtrlMode_INIT;
+	boolean IsUpdated;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatModeBtn_SeatModeBtn(&SeatModeBtn);
+	IsUpdated = Rte_IsUpdated_rpSeatModeBtn_SeatModeBtn();
+	if (status == RTE_E_OK && IsUpdated == TRUE)
+	{
+		switch(SeatModeBtn)
+		{
+			case (SEAT_MODE_BUTTON_MANUAL):
+				SeatCtrlMode = RTE_MODE_SeatCtrlMode_MANUAL;
+				break;
+			case (SEAT_MODE_BUTTON_AUTO):
+				SeatCtrlMode = RTE_MODE_SeatCtrlMode_AUTO;
+				break;
+			default:
+				SeatCtrlMode = RTE_MODE_SeatCtrlMode_INIT;
+				break;
+		}
+	}
+	
+	/* Mode Switch Points */
+	(void)Rte_Switch_ppSeatCtrlMode_SeatCtrlMode(SeatCtrlMode);
+	
 }
 
